5d23e558913b2eded27bd29b9130cc0093388ea8
Janice <janiceyohana@gmail.com>
2024-10-26 21:49:55 +0800

    merge app.js

--- spatial-fe/src/App.js
+++ spatial-fe/src/App.js
@@ -1,21 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ExhibitsList from './pages/exhibits-list.jsx';
 import ExhibitDesign from './pages/exhibit-design.jsx';
 import Guidelines from './pages/guidelines.jsx';
 import NavBar from './components/navbar.jsx';
+import Sidebar from './components/sidebar';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <Router>
-      <NavBar />
-      <Routes>
+      <NavBar onSidebarToggle={toggleSidebar} />
+
+      {/* Sidebar positioned below the NavBar */}
+      <div className={`fixed top-[78px] left-0 z-50 transition-transform ease-in-out duration-500 ${isSidebarOpen ? 'block' : 'hidden'}`}>
+        <Sidebar />
+      </div>
+
+      {/* Overlay for the rest of the page */}
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black opacity-30 z-40"
+          style={{
+            top: '78px',
+            left: '355px',
+            height: 'calc(100% - 78px)',
+            width: 'calc(100% - 355px)',
+          }}
+          onClick={toggleSidebar}
+        />
+      )}
+
+      {/* Main content */}
+      <div className="flex-1 transition-all duration-300 ml-0">
+        <Routes>
           <Route index element={<ExhibitsList />} /> {/* Default route for "/" */}
           <Route path="/exhibitions" element={<ExhibitsList />} />
           <Route path="/exhibitions/:id" element={<ExhibitDesign />} />
           <Route path="/guidelines" element={<Guidelines />} />
-      </Routes>
+        </Routes>
+      </div>
     </Router>
   );
 }
