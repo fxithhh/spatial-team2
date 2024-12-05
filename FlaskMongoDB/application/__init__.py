63f3078247ca74945f16f487d2fbad196e44e20a
jordanleewei <94658443+jordanleewei@users.noreply.github.com>
2024-12-05 17:37:11 +0800

    Update __init__.py

--- FlaskMongoDB/application/__init__.py
+++ FlaskMongoDB/application/__init__.py
@@ -22,3 +22,6 @@ except Exception as e:
     print(f"Could not connect to MongoDB: {e}")
 
 from application import routes
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
